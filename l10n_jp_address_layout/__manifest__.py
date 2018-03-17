591b314f11dcd900df5ec32bf6347cb42df22179
Yoshi Tashiro <tashiro@roomsfor.hk>
2018-03-17 19:43:01 +0800

    11.0 add l10n jp address layout (#1)
    * [ADD] l10n_jp_address_layout
    
    * add readme

---
+++ l10n_jp_address_layout/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    'name': 'Japan Address Layout',
+    'version': '11.0.1.0.0',
+    'depends': [
+        'base',
+    ],
+    'author': 'Quartile Limited, '
+              'Odoo Community Association (OCA)',
+    'license': "LGPL-3",
+    'website': 'https://odoo-community.org/',
+    'category': 'Extra Tools',
+    'description': """
+This module provides Japan address input field layout.
+    """,
+    'data': [
+        'views/res_partner_views.xml',
+        'data/res_country_data.xml',
+    ],
+    'installable': True,
+}
