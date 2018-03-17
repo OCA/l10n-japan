95b643910845ae997a599dd92d794bb32253f14d
Yoshi Tashiro <tashiro@quartile.co>
2018-03-17 15:35:03 +0000

    [ADD] l10n_jp_country_state

---
+++ l10n_jp_country_state/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    'name': 'Japan Country States',
+    'version': '11.0.1.0.0',
+    'depends': [
+        'base_country_state_translatable',
+    ],
+    'author': 'Quartile Limited, '
+              'Odoo Community Association (OCA)',
+    'license': "LGPL-3",
+    'website': 'https://odoo-community.org/',
+    'category': 'Localization',
+    'description': """
+This module updates Japanese prefecture data with translation.
+    """,
+    'data': [
+        'data/res_country_state.xml',
+    ],
+    'installable': True,
+}
