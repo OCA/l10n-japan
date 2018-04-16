d2ad1ffdec1651af7c72d2839e28816ed3963334
Yoshi Tashiro <tashiro@quartile.co>
2018-04-16 15:15:19 +0000

    [FIX] version

--- l10n_jp_partner_title_qweb/__manifest__.py
+++ l10n_jp_partner_title_qweb/__manifest__.py
@@ -3,7 +3,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     'name': 'Japan Partner Title QWeb',
-    'version': '11.0.1.1.0',
+    'version': '11.0.1.0.0',
     'depends': [
         'base',
     ],
