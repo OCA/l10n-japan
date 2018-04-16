236208a119584b378af66c7ff1858d07bdba2d88
Yoshi Tashiro <tashiro@quartile.co>
2018-04-16 08:34:01 +0000

    [IMP] add constraint

--- l10n_jp_partner_title_qweb/models/res_partner_title.py
+++ l10n_jp_partner_title_qweb/models/res_partner_title.py
@@ -2,7 +2,8 @@
 # Copyright 2018 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import models, fields
+from odoo import api, models, fields, _
+from odoo.exceptions import ValidationError
 
 
 class ResPartnerTitle(models.Model):
@@ -23,3 +24,13 @@ class ResPartnerTitle(models.Model):
         help='If selected, this title should be proposed to company partners '
              'according to the language selection of the partner.',
     )
+
+    @api.constrains('lang_id', 'for_company')
+    def _check_lang_id_for_company(self):
+        for record in self:
+            if record.lang_id and self.search_count(
+                    [('lang_id', '=', record.lang_id.id),
+                     ('for_company', '=', record.for_company)]) > 1:
+                raise ValidationError(_('You cannot have more than one record '
+                                        'for a combination of "Language" and '
+                                        '"For Company".'))
