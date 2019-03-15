58669ba0bc2f864d286a9867d4bf6d4a695485ce
Yoshi Tashiro <tashiro@quartile.co>
2019-03-15 22:57:45 +0800

    [MIG] l10n_jp_country_state: Migration to 12.0

--- l10n_jp_country_state/__manifest__.py
+++ l10n_jp_country_state/__manifest__.py
@@ -1,16 +1,15 @@
-# -*- coding: utf-8 -*-
-# Copyright 2018 Quartile Limited
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# Copyright 2018-2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Japan Country States',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'depends': [
         'base_country_state_translatable',
     ],
     'author': 'Quartile Limited, '
               'Odoo Community Association (OCA)',
-    'license': "LGPL-3",
-    'website': 'https://odoo-community.org/',
+    'license': "AGPL-3",
+    'website': 'https://github.com/OCA/l10n-japan',
     'category': 'Localization',
     'description': """
 This module updates Japanese prefecture data with translation.
