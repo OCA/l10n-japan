6f1f33a52e8170888a9ddcace99c7efabf849bf1
Yoshi Tashiro <tashiro@quartile.co>
2019-04-30 12:45:31 +0800

    [MIG] l10n_jp_partner_title_qweb: Migration to 12.0
    update translation
    
    update readme

--- l10n_jp_partner_title_qweb/tests/test_l10n_jp_partner_title.py
+++ l10n_jp_partner_title_qweb/tests/test_l10n_jp_partner_title.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# Copyright 2018 Quartile Limited
+# Copyright 2018-2019 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from odoo.tests import common
@@ -18,8 +17,14 @@ class L10nJpPartnerTitleQwebCase(common.TransactionCase):
             'l10n_jp_partner_title_qweb.res_partner_title_onchuu')
         # company partner
         self.partner1 = self.env.ref('base.res_partner_1')
-        # individual partner
-        self.partner_address1 = self.env.ref('base.res_partner_address_1')
+        # individual partner not linked to user
+        self.partner2 = self.env.ref('base.res_partner_address_1')
+        # individual partner linked to non-internal user
+        self.partner3 = self.env.ref('base.partner_demo_portal')
+        # individual partner linked to internal user
+        self.partner4 = self.env.ref('base.partner_demo')
+        # partner for main company
+        self.partner5 = self.env.ref('base.main_company').partner_id
 
     def test_00_title_constraint(self):
         with self.assertRaises(ValidationError):
@@ -39,6 +44,21 @@ class L10nJpPartnerTitleQwebCase(common.TransactionCase):
         self.assertEqual(partner.title, self.title_onchuu)
 
     def test_03_title_proposal(self):
-        partner = self.partner_address1
+        partner = self.partner2
         partner.lang = self.japanese.code
         self.assertEqual(partner.title, self.title_sama)
+
+    def test_04_title_proposal(self):
+        partner = self.partner3
+        partner.lang = self.japanese.code
+        self.assertEqual(partner.title, self.title_sama)
+
+    def test_05_title_proposal(self):
+        partner = self.partner4
+        partner.lang = self.japanese.code
+        self.assertFalse(partner.title)
+
+    def test_06_title_proposal(self):
+        partner = self.partner5
+        partner.lang = self.japanese.code
+        self.assertFalse(partner.title)
