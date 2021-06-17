7372fc457eace7e28e58f72b78b704ef9a573eed
TomohiroAbe-Quartile <tomohiro@quartile.co>
2021-06-17 15:01:33 +0800

    delete data and change version 12 to 13

--- l10n_jp_country_state/__manifest__.py
+++ l10n_jp_country_state/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Japan Country States",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "depends": ["base_country_state_translatable",],
     "author": "Quartile Limited, " "Odoo Community Association (OCA)",
     "license": "AGPL-3",
@@ -11,6 +11,5 @@
     "description": """
 This module updates Japanese prefecture data with translation.
     """,
-    "data": ["data/res_country_state.xml",],
     "installable": True,
 }
