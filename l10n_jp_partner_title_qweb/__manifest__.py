333ef09b0edf9a17e6ec83541fd79310c132fc89
Ryoko <ryoko@quartile.co>
2021-09-24 14:31:35 +0800

    [MIG] l10n_jp_partner_title_qweb: Migration to 13.0

--- l10n_jp_partner_title_qweb/__manifest__.py
+++ l10n_jp_partner_title_qweb/__manifest__.py
@@ -1,25 +1,18 @@
 # Copyright 2018-2019 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
-    'name': 'Japan Partner Title QWeb',
-    'version': '12.0.1.0.0',
-    'depends': [
-        'base',
+    "name": "Japan Partner Title QWeb",
+    "version": "13.0.1.0.0",
+    "depends": ["base"],
+    "author": "Quartile Limited, " "Odoo Community Association (OCA)",
+    "license": "LGPL-3",
+    "website": "https://github.com/OCA/l10n-japan",
+    "category": "Localization",
+    "data": [
+        "views/res_partner_title_views.xml",
+        "views/res_partner_views.xml",
+        "report/templates.xml",
+        "data/res_partner_title_data.xml",
     ],
-    'author': 'Quartile Limited, '
-              'Odoo Community Association (OCA)',
-    'license': "LGPL-3",
-    'website': 'https://github.com/OCA/l10n-japan',
-    'category': 'Localization',
-    'description': """
-The module extends the default partner title model to allow the title
-to be shown in most printed reports.
-    """,
-    'data': [
-        'views/res_partner_title_views.xml',
-        'views/res_partner_views.xml',
-        'report/templates.xml',
-        'data/res_partner_title_data.xml',
-    ],
-    'installable': True,
+    "installable": True,
 }
