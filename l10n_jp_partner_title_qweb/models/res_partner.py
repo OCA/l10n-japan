e1d353f9907feda188bcc3ebb030542f6ee724ba
Ryoko <ryoko@quartile.co>
2021-09-24 14:31:35 +0800

    [MIG] l10n_jp_partner_title_qweb: Migration to 13.0

--- l10n_jp_partner_title_qweb/models/res_partner.py
+++ l10n_jp_partner_title_qweb/models/res_partner.py
@@ -5,34 +5,39 @@ from odoo import api, fields, models
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
     # we make title a computed field instead of using onchange to update it
     # because onchange is not triggered when a user signs up from portal
-    title = fields.Many2one(
-        compute='_compute_title',
-        store=True,
-        readonly=False,
-    )
+    title = fields.Many2one(compute="_compute_title", store=True, readonly=False,)
 
-    @api.multi
-    @api.depends('lang', 'is_company')
+    @api.depends("lang", "is_company")
     def _compute_title(self):
         for partner in self:
             # handle the case NewId is returned for partner.id (i.e. onchange)
-            partner_id = self._origin.id if isinstance(
-                partner.id, models.NewId) else partner.id
+            partner_id = (
+                self._origin.id if isinstance(partner.id, models.NewId) else partner.id
+            )
             # no title proposal for partners linked to internal users and
             # companies (res.company)
-            if not partner.lang or partner.user_ids and \
-                    not partner.user_ids.share or \
-                    self.env['res.company'].search(
-                        [('partner_id', '=', partner_id)]):
+            if (
+                not partner.lang
+                or partner.user_ids
+                and not partner.user_ids.share
+                or self.env["res.company"].search([("partner_id", "=", partner_id)])
+            ):
                 partner.title = False
             else:
-                lang_id = self.env['res.lang'].search(
-                    [('code', '=', partner.lang)], limit=1).id
-                title = self.env['res.partner.title'].search(
-                    [('lang_id', '=', lang_id),
-                     ('for_company', '=', partner.is_company)], limit=1)
+                lang_id = (
+                    self.env["res.lang"]
+                    .search([("code", "=", partner.lang)], limit=1)
+                    .id
+                )
+                title = self.env["res.partner.title"].search(
+                    [
+                        ("lang_id", "=", lang_id),
+                        ("for_company", "=", partner.is_company),
+                    ],
+                    limit=1,
+                )
                 partner.title = title.id if title else False
