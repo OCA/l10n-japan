4abb4a3984517325c328be1ee23a676891ff6398
Aungkokolin1997 <aungkokolin23397@gmail.com>
2022-10-18 15:17:48 +0630

    [MIG] l10n_jp_address_layout: Migration to 15.0

--- l10n_jp_address_layout/__manifest__.py
+++ l10n_jp_address_layout/__manifest__.py
@@ -1,17 +1,21 @@
-# Copyright 2018-2019 Quartile Limited
+# Copyright 2022 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Japan Address Layout",
-    "version": "13.0.1.0.1",
+    "version": "15.0.1.0.0",
     "depends": ["web"],
     "author": "Quartile Limited, " "Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "website": "https://github.com/OCA/l10n-japan",
     "category": "Localization",
     "data": [
-        "views/assets.xml",
         "views/res_partner_views.xml",
         "data/res_country_data.xml",
     ],
+    "assets": {
+        "web.assets_backend": [
+            "l10n_jp_address_layout/static/src/scss/form_view.scss",
+        ],
+    },
     "installable": True,
 }
