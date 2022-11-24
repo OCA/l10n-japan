c73821d3902f08f43e8effdd99347586ca350d02
Aungkokolin1997 <aungkokolin23397@gmail.com>
2022-11-24 17:25:22 +0630

    [MIG] l10n_jp_address_layout: Migration to 16.0

--- l10n_jp_address_layout/__manifest__.py
+++ l10n_jp_address_layout/__manifest__.py
@@ -1,8 +1,8 @@
 # Copyright 2022 Quartile Limited
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 {
     "name": "Japan Address Layout",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "depends": ["web"],
     "author": "Quartile Limited, " "Odoo Community Association (OCA)",
     "license": "LGPL-3",
@@ -12,10 +12,5 @@
         "views/res_partner_views.xml",
         "data/res_country_data.xml",
     ],
-    "assets": {
-        "web.assets_backend": [
-            "l10n_jp_address_layout/static/src/scss/form_view.scss",
-        ],
-    },
     "installable": True,
 }
--- l10n_jp_address_layout/readme/USAGE.rst
+++ l10n_jp_address_layout/readme/USAGE.rst
@@ -1,4 +1,4 @@
 The address layout of the partner form will look as shown in below image.
 
-.. figure:: static/description/japan_address_layout.png
+.. figure:: ../static/description/japan_address_layout.png
    :alt: Address layout
