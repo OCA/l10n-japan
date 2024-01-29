aaaa79a8944d6331c249b3b976ba228e26c390f3
Aungkokolin1997 <lin@quartile.co>
2024-01-29 16:54:32 +0630

    [ADD] l10n_jp_partner_zip_address

---
+++ l10n_jp_partner_zip_address/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2024 Quartile Limited
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Japan Partner Zip Address",
+    "version": "15.0.1.0.0",
+    "author": "Quartile Limited, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/l10n-japan",
+    "category": "Localization",
+    "depends": ["base"],
+    "external_dependencies": {"python": ["jaconv"]},
+    "installable": True,
+}
