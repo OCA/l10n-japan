361e57fcdd2bd9e6e3fe7a595101ed388e992666
Aungkokolin1997 <lin@quartile.co>
2024-10-24 04:12:18 +0000

    [ADD] report_alternative_layout

---
+++ report_alternative_layout/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2024 Quartile
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Report Alternative Layout",
+    "version": "16.0.1.0.0",
+    "category": "Reporting",
+    "license": "AGPL-3",
+    "author": "Quartile, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-japan",
+    "depends": ["base"],
+    "data": [
+        "data/report_paperformat_data.xml",
+        "report/report_template.xml",
+        "views/ir_actions_report_views.xml",
+    ],
+    "installable": True,
+}
---
+++ report_alternative_layout/readme/CONFIGURE.rst
@@ -0,0 +1,8 @@
+Go to *Settings > Technical > Reporting > Reports*, and open a report of your choice, and
+update the following fields as necessary.
+
+- Apply Alternative Layout: If selected, the alternative layout will be applied in the
+  printed report.
+- Paper Format: Select 'A4 - no company address in header' if 'Apply Alternative Layout' is selected.
+- Show Commercial Partner: If selected, the commercial partner of the document partner
+  will show in the report output (instead of the document partner).
