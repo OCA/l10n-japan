ae0a0ac2656d11c603b033dc13d69d6b8c00e587
Aungkokolin1997 <lin@quartile.co>
2025-03-18 03:47:11 +0000

    [IMP] report_alternative_layout: improve remit_to_bank

--- report_alternative_layout/models/ir_actions_report.py
+++ report_alternative_layout/models/ir_actions_report.py
@@ -1,7 +1,7 @@
 # Copyright 2024-2025 Quartile (https://www.quartile.co)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class Report(models.Model):
@@ -32,10 +32,30 @@ class Report(models.Model):
             return partner.commercial_partner_id
         return partner
 
+    @api.model
+    def _get_bank_field_name(self, record):
+        """Get the name of the field that links the record to the bank account.
+
+        We assume that there is usually just one field in the model with many2one
+        relationship to res.partner.bank. In case of an exception, this method should be
+        extended in the specific model to identify the correct field.
+        """
+        return next(
+            (
+                field.name
+                for field in record._fields.values()
+                if field.type == "many2one" and field.comodel_name == "res.partner.bank"
+            ),
+            None,
+        )
+
     def _get_remit_to_bank(self, record):
         self.ensure_one()
         if not self.show_remit_to_bank:
             return False
+        bank_field_name = self._get_bank_field_name(record)
+        if bank_field_name:
+            return getattr(record, bank_field_name)
         if "company_id" not in record._fields:
             return False
         company = record.company_id
