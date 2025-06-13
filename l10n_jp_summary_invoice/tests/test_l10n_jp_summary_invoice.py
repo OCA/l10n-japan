3322fb78d3092fee44e23b9ce44449a678c2998c
Aungkokolin1997 <lin@quartile.co>
2025-06-13 08:41:38 +0000

    [FIX] l10n_jp_summary_invoice: Correct handling of credit notes
    Previously, credit notes were incorrectly treated as positive amounts
    in reporting and tax computation, just like regular invoices. This
    commit addresses the issue by introducing the `signed_quantity` field
    on `account.move.line` and refactoring the `_compute_tax_totals()`
    method to delegate tax computation to Odoo’s standard logic in
    `account.move`.
    
    Minor improvements were also made to the generated tax adjustment
    entries, including setting the correct due date and enhancing the
    reference field.

--- l10n_jp_summary_invoice/tests/test_l10n_jp_summary_invoice.py
+++ l10n_jp_summary_invoice/tests/test_l10n_jp_summary_invoice.py
@@ -1,4 +1,4 @@
-# Copyright 2024 Quartile
+# Copyright 2024-2025 Quartile
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo.exceptions import ValidationError
@@ -58,13 +58,13 @@ class TestSummaryInvoice(TransactionCase):
             }
         )
 
-    def _create_invoice(self, amount, tax, bank=None):
+    def _create_invoice(self, amount, tax, move_type="out_invoice", bank=None):
         invoice = (
             self.env["account.move"]
             .with_company(self.company)
             .create(
                 {
-                    "move_type": "out_invoice",
+                    "move_type": move_type,
                     "partner_id": self.partner.id,
                     "partner_bank_id": bank and bank.id,
                     "invoice_line_ids": [
@@ -83,6 +83,16 @@ class TestSummaryInvoice(TransactionCase):
         )
         invoice.action_post()
         return invoice
+
+    def _get_billing_tax_amount(self, billing):
+        tax_totals = billing.tax_totals
+        groups_by_subtotal = tax_totals.get("groups_by_subtotal", {})
+        key = next(iter(groups_by_subtotal))
+        tax_group_amount_dict = {
+            entry["tax_group_id"]: entry["tax_group_amount"] * -1
+            for entry in groups_by_subtotal[key]
+        }
+        return round(tax_group_amount_dict.get(self.tax_10.tax_group_id.id, 0), 0)
 
     def test_get_moves_filters_billed_and_flags(self):
         invoice = self._create_invoice(50, self.tax_10)
@@ -146,30 +156,41 @@ class TestSummaryInvoice(TransactionCase):
         self.assertEqual(billing.remit_to_bank_id, self.bank_account)
 
     def test_create_tax_adjustment_entry(self):
-        inv_1 = self._create_invoice(101, self.tax_10)
-        inv_2 = self._create_invoice(102, self.tax_10)
-        inv_3 = self._create_invoice(103, self.tax_10)
-        self.assertEqual(inv_1.amount_tax, 10)
-        self.assertEqual(inv_2.amount_tax, 10)
-        self.assertEqual(inv_3.amount_tax, 10)
-        invoices = inv_1 + inv_2 + inv_3
+        out_inv_1 = self._create_invoice(102, self.tax_10)
+        out_inv_2 = self._create_invoice(102, self.tax_10)
+        out_inv_3 = self._create_invoice(102, self.tax_10)
+        self.assertEqual(out_inv_1.amount_tax, 10)
+        self.assertEqual(out_inv_2.amount_tax, 10)
+        self.assertEqual(out_inv_3.amount_tax, 10)
+        invoices = out_inv_1 + out_inv_2 + out_inv_3
         action = invoices.action_create_billing()
         billing = self.env["account.billing"].browse(action["res_id"])
         self.assertEqual(billing.state, "draft")
         billing.with_company(self.company).validate_billing()
-        tax_totals = billing.tax_totals
-        groups_by_subtotal = tax_totals.get("groups_by_subtotal", {})
-        key = next(iter(groups_by_subtotal))
-        tax_group_amount_dict = {
-            entry["tax_group_id"]: entry["tax_group_amount"] * -1
-            for entry in groups_by_subtotal[key]
-        }
-        billing_tax_amount = round(
-            tax_group_amount_dict.get(self.tax_10.tax_group_id.id, 0), 0
-        )
+        billing_tax_amount = self._get_billing_tax_amount(billing)
+        # The total tax amount should be 31 (306 * 0.1)
         self.assertEqual(abs(billing_tax_amount), 31)
-        tax_adjustment_entry = billing.tax_adjustment_entry_id
-        self.assertTrue(
-            tax_adjustment_entry, "Tax adjustment journal entry should be created."
-        )
-        self.assertEqual(tax_adjustment_entry.amount_total_signed, 1)
+        tax_adj_entry = billing.tax_adjustment_entry_id
+        # Since the total tax amount of the billing (31) is different from the total tax
+        # amount of the invoices (30), a tax adjustment entry should be created.
+        self.assertTrue(tax_adj_entry)
+        self.assertEqual(tax_adj_entry.amount_total_signed, 1)
+        billing.action_cancel()
+        self.assertTrue(tax_adj_entry.state, "cancel")
+        self.assertFalse(billing.tax_adjustment_entry_id)
+        # Add a credit note to the billing
+        out_ref = self._create_invoice(102, self.tax_10, "out_refund")
+        self.assertEqual(out_ref.amount_tax, 10)
+        billing.action_cancel_draft()
+        line_vals = {
+            "move_id": out_ref.id,
+            "amount_total": -out_ref.amount_total,
+            "amount_residual": -out_ref.amount_residual,
+        }
+        billing.write({"billing_line_ids": [Command.create(line_vals)]})
+        self.assertIn(out_ref, billing.billing_line_ids.move_id)
+        billing.with_company(self.company).validate_billing()
+        billing_tax_amount = self._get_billing_tax_amount(billing)
+        # The total tax amount should be 20 (204 * 0.1)
+        self.assertEqual(abs(billing_tax_amount), 20)
+        self.assertFalse(billing.tax_adjustment_entry_id)
