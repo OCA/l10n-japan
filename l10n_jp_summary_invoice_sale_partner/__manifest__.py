5e18cd661749b06292520e6fee4469a4ceaec1ed
Aungkokolin1997 <lin@quartile.co>
2025-08-07 07:40:07 +0000

    [ADD] l10n_jp_summary_invoice_sale_partner

---
+++ l10n_jp_summary_invoice_sale_partner/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2025 Quartile (https://www.quartile.co)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Japan Summary Invoice Sale Partner",
+    "version": "16.0.1.0.0",
+    "category": "Japanese Localization",
+    "author": "Quartile, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-japan",
+    "license": "AGPL-3",
+    "depends": ["account_move_sale_partner", "l10n_jp_summary_invoice"],
+    "data": [
+        "reports/report_summary_invoice_templates.xml",
+        "views/res_config_settings_views.xml",
+    ],
+    "development_status": "Alpha",
+    "installable": True,
+}
