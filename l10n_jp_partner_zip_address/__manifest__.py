f8b5f7764773a2dab1f943a1e404abce6548522a
Aungkokolin1997 <lin@quartile.co>
2025-08-14 02:21:58 +0000

    [MIG] l10n_jp_partner_zip_address: Migration to 17.0

--- l10n_jp_partner_zip_address/__manifest__.py
+++ l10n_jp_partner_zip_address/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Japan Partner Zip Address",
-    "version": "18.0.1.0.1",
+    "version": "17.0.1.0.0",
     "author": "Quartile, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "website": "https://github.com/OCA/l10n-japan",
