10eab7ebbf967b096d2f8903a0fa2347cb87c28b
Aungkokolin1997 <lin@quartile.co>
2025-09-01 01:06:44 +0000

    [IMP] account_payment_term_cutoff_day: add cutoff_date

--- account_payment_term_cutoff_day/__manifest__.py
+++ account_payment_term_cutoff_day/__manifest__.py
@@ -9,6 +9,9 @@
     "license": "AGPL-3",
     "website": "https://github.com/OCA/l10n-japan",
     "category": "Accounting",
-    "data": ["views/account_payment_term_views.xml"],
+    "data": [
+        "views/account_payment_term_views.xml",
+        "views/account_move_views.xml",
+    ],
     "installable": True,
 }
--- account_payment_term_cutoff_day/tests/test_account_invoice_date_due.py
+++ account_payment_term_cutoff_day/tests/test_account_invoice_date_due.py
@@ -13,57 +13,55 @@ class TestPaymentTermCutoffDate(TransactionCase):
         cls.payment_term = cls.env["account.payment.term"].create(
             {"name": "Test Payment Term"}
         )
-        cls.payment_term_line = cls.env["account.payment.term.line"].create(
+        # There should be a default line
+        cls.payment_term_line = cls.payment_term.line_ids
+        cls.payment_term_line.write(
+            {"value": "balance", "months": 1, "end_month": True, "cutoff_day": 20}
+        )
+
+    def _create_invoice(self, invoice_date):
+        return self.env["account.move"].create(
             {
-                "payment_id": cls.payment_term.id,
-                "value": "balance",
-                "months": 1,
-                "end_month": True,
-                "cutoff_day": 20,
+                "move_type": "out_invoice",
+                "partner_id": self.env.ref("base.res_partner_1").id,
+                "invoice_date": invoice_date,
+                "invoice_payment_term_id": self.payment_term.id,
             }
         )
 
     def test_due_date_before_cutoff_day(self):
-        """Test if due date remains unchanged when invoice date is
-        before or equal to cutoff_day"""
-        invoice_date = date(2024, 2, 20)
-        expected_due_date = date(2024, 3, 31)
-        computed_due_date = self.payment_term_line._get_due_date(invoice_date)
-        self.assertEqual(
-            computed_due_date,
-            expected_due_date,
-        )
+        """Test if due date remains unchanged when invoice date is before or equal to
+        cutoff_day
+        """
+        computed_due_date = self.payment_term_line._get_due_date(date(2024, 2, 20))
+        self.assertEqual(computed_due_date, date(2024, 3, 31))
 
     def test_due_date_after_cutoff_day(self):
-        """Test if due date is shifted by +1 month
-        when invoice date is after cutoff_day"""
-        invoice_date = date(2024, 2, 21)
-        expected_due_date = date(2024, 4, 30)
-        computed_due_date = self.payment_term_line._get_due_date(invoice_date)
-        self.assertEqual(
-            computed_due_date,
-            expected_due_date,
-        )
+        """Test if due date is shifted by +1 month when invoice date is after cutoff_day"""
+        computed_due_date = self.payment_term_line._get_due_date(date(2024, 2, 21))
+        self.assertEqual(computed_due_date, date(2024, 4, 30))
 
     def test_due_date_after_cutoff_day_with_31_days(self):
         """Test if due date is shifted by +1 month and the following month has 31 days
-        when invoice date is after cutoff_day"""
-        invoice_date = date(2025, 1, 31)
-        expected_due_date = date(2025, 3, 31)
-        computed_due_date = self.payment_term_line._get_due_date(invoice_date)
-        self.assertEqual(
-            computed_due_date,
-            expected_due_date,
-        )
+        when invoice date is after cutoff_day
+        """
+        computed_due_date = self.payment_term_line._get_due_date(date(2025, 1, 31))
+        self.assertEqual(computed_due_date, date(2025, 3, 31))
 
     def test_due_date_no_cutoff_day(self):
         """Test if due date remains unchanged when cutoff_day is not set"""
         self.payment_term_line.cutoff_day = False
-        invoice_date = date(2024, 2, 21)
-        expected_due_date = date(2024, 3, 31)
-        computed_due_date = self.payment_term_line._get_due_date(invoice_date)
+        computed_due_date = self.payment_term_line._get_due_date(date(2024, 2, 21))
         self.assertEqual(
             computed_due_date,
-            expected_due_date,
+            date(2024, 3, 31),
             "Due date should not change when cutoff_day is not set.",
         )
+
+    def test_invoice_cutoff_date(self):
+        inv = self._create_invoice(date(2025, 9, 15))
+        self.assertEqual(inv.cutoff_date, date(2025, 9, 20))
+        inv.invoice_date = date(2025, 9, 21)
+        self.assertEqual(inv.cutoff_date, date(2025, 10, 20))
+        inv.invoice_payment_term_id = False
+        self.assertEqual(inv.cutoff_date, date(2025, 9, 21))
