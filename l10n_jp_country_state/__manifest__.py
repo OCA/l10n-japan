4199c6ac4aae3b66d290a667a063bec64fa71781
Aungkokolin1997 <lin@quartile.co>
2025-11-20 04:40:00 +0000

    [MIG] l10n_jp_country_state: Migration to 19.0

--- l10n_jp_country_state/__manifest__.py
+++ l10n_jp_country_state/__manifest__.py
@@ -2,11 +2,12 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Japan Country States",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "depends": ["base_country_state_translatable"],
     "author": "Quartile, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "website": "https://github.com/OCA/l10n-japan",
     "category": "Localization",
+    "maintainers": ["yostashiro", "aungkokolin1997"],
     "installable": True,
 }
