c1e43c3d9851d68ea692bad9fb7c9748b53de38c
sanami <sanami@quartile.co>
2026-03-31 09:00:38 +0000

    [MIG] account_move_delivery_invoice: Migration to 19.0

--- account_move_delivery_invoice/__manifest__.py
+++ account_move_delivery_invoice/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Account Move Delivery Invoice",
-    "version": "18.0.1.2.0",
+    "version": "19.0.1.0.0",
     "depends": ["account"],
     "author": "Quartile, Odoo Community Association (OCA)",
     "license": "AGPL-3",
