0065707ae21b1b0e162077360217e86629a1ebcb
Aungkokolin1997 <lin@quartile.co>
2026-06-30 08:09:17 +0000

    add summary

--- l10n_jp_summary_invoice_order_partner/__manifest__.py
+++ l10n_jp_summary_invoice_order_partner/__manifest__.py
@@ -2,6 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Japan Summary Invoice Order Partner",
+    "summary": "Show sale order partner and address in summary invoice",
     "version": "18.0.1.0.0",
     "category": "Japanese Localization",
     "author": "Quartile, Odoo Community Association (OCA)",
